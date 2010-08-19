94d84c3c516a287be1b4ce5379f7d0e3992f016e
Andrew Woods <awoods@duraspace.org>
2010-08-19 23:50:26 +0000

    This update adds the StorageProviderFactory interface that was left out of the previous checkin.
    git-svn-id: https://svn.duraspace.org/duracloud/trunk@90 1005ed41-97cd-4a8f-848c-be5b5fe45bcb

---
+++ durastore/src/main/java/org/duracloud/durastore/util/StorageProviderFactory.java
@@ -0,0 +1,39 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duracloud.durastore.util;
+
+import org.duracloud.storage.domain.StorageProviderType;
+import org.duracloud.storage.error.StorageException;
+import org.duracloud.storage.provider.StatelessStorageProvider;
+import org.duracloud.storage.provider.StorageProvider;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+/**
+ * @author Andrew Woods
+ *         Date: Aug 19, 2010
+ */
+public interface StorageProviderFactory {
+
+    public void initialize(InputStream accountXml);
+
+    public Iterator<String> getStorageProviderAccountIds()
+        throws StorageException;
+
+    public String getPrimaryStorageProviderAccountId() throws StorageException;
+
+    public StorageProvider getStorageProvider() throws StorageException;
+
+    public StorageProvider getStorageProvider(String storageAccountId)
+        throws StorageException;
+
+    public StorageProviderType getStorageProviderType(String storageAccountId)
+        throws StorageException;
+
+}
