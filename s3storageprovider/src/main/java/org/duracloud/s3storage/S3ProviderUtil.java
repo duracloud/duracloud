1423148da4b401c4eeb135c1780e73f93c0d5ddc
Andrew Woods <awoods@duraspace.org>
2010-08-23 13:46:24 +0000

    This update is a follow-on to https://jira.duraspace.org/browse/DURACLOUD-164 . It caches S3Services and CloudFrontServices instead of creating new ones for each REST request. These AWS classes consume resources without releasing them unless an explicit shutdown() call is made.
    git-svn-id: https://svn.duraspace.org/duracloud/trunk@96 1005ed41-97cd-4a8f-848c-be5b5fe45bcb

--- s3storageprovider/src/main/java/org/duracloud/s3storage/S3ProviderUtil.java
+++ s3storageprovider/src/main/java/org/duracloud/s3storage/S3ProviderUtil.java
@@ -8,7 +8,6 @@
 package org.duracloud.s3storage;
 
 import org.duracloud.storage.error.StorageException;
-import static org.duracloud.storage.error.StorageException.RETRY;
 import org.jets3t.service.CloudFrontService;
 import org.jets3t.service.CloudFrontServiceException;
 import org.jets3t.service.S3Service;
@@ -16,26 +15,65 @@ import org.jets3t.service.S3ServiceException;
 import org.jets3t.service.impl.rest.httpclient.RestS3Service;
 import org.jets3t.service.security.AWSCredentials;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.duracloud.storage.error.StorageException.RETRY;
+
 /**
  * @author: Bill Branan
  * Date: May 20, 2010
  */
 public class S3ProviderUtil {
 
+    private static Map<String, S3Service> s3Services = new HashMap<String, S3Service>();
+    private static Map<String, CloudFrontService> cloudFrontServices = new HashMap<String, CloudFrontService>();
+
     public static S3Service getS3Service(String accessKey, String secretKey) {
-        AWSCredentials awsCredentials = new AWSCredentials(accessKey, secretKey);
+        S3Service service = s3Services.get(key(accessKey, secretKey));
+        if (null == service) {
+            service = newS3Service(accessKey, secretKey);
+        }
+
+        s3Services.put(key(accessKey, secretKey), service);
+        return service;
+    }
+
+    private static String key(String accessKey, String secretKey) {
+        return accessKey + secretKey;
+    }
+
+    private static S3Service newS3Service(String accessKey, String secretKey) {
+        S3Service service;
+        AWSCredentials awsCredentials = new AWSCredentials(accessKey,
+                                                           secretKey);
         try {
-            return new RestS3Service(awsCredentials);
+            service = new RestS3Service(awsCredentials);
         } catch (S3ServiceException e) {
-            String err = "Could not create connection to Amazon S3 due " +
-                         "to error: " + e.getMessage();
+            String err =
+                "Could not create connection to Amazon S3 due " + "to error: " +
+                    e.getMessage();
             throw new StorageException(err, e, RETRY);
         }
+        return service;
     }
 
     public static CloudFrontService getCloudFrontService(String accessKey,
                                                          String secretKey) {
-        AWSCredentials awsCredentials = new AWSCredentials(accessKey, secretKey);
+        CloudFrontService service = cloudFrontServices.get(key(accessKey,
+                                                               secretKey));
+        if (null == service) {
+            service = newCloudFrontService(accessKey, secretKey);
+        }
+
+        cloudFrontServices.put(key(accessKey, secretKey), service);
+        return service;
+    }
+
+    private static CloudFrontService newCloudFrontService(String accessKey,
+                                                          String secretKey) {
+        AWSCredentials awsCredentials = new AWSCredentials(accessKey,
+                                                           secretKey);
         try {
             return new CloudFrontService(awsCredentials);
         } catch (CloudFrontServiceException e) {
