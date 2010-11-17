b33ab2135beae48129fda554d558c96cff4ad843
Daniel Bernstein <dbernstein@duraspace.org>
2010-11-17 19:18:32 +0000

    using placeholder for default filter text
    git-svn-id: https://svn.duraspace.org/duracloud/trunk@233 1005ed41-97cd-4a8f-848c-be5b5fe45bcb

--- duradmin/src/main/webapp/javascript/spaces-manager.js
+++ duradmin/src/main/webapp/javascript/spaces-manager.js
@@ -1708,11 +1708,7 @@ $(document).ready(function() {
 	};
 	
 	var getFilterText = function(){
-		var val = $("#content-item-filter").val();
-		if(val == DEFAULT_FILTER_TEXT){
-			val = null;
-		}
-		return val;
+		return $("#content-item-filter").val();
 	};
 	
 	var getSpace = function(spaceId, loadHandler){
@@ -2106,18 +2102,6 @@ $(document).ready(function() {
 	$("#content-item-list").selectablelist({selectable: true});
 	$("#spaces-list").selectablelist({selectable: true});
 
-	var DEFAULT_FILTER_TEXT = "filter";
-
-	$(".dc-item-list-filter").focus(function(){
-		if($(this).val() == DEFAULT_FILTER_TEXT){
-			$(this).val('');
-		};
-	}).blur(function(){
-		if($(this).val() == ""){
-			$(this).val(DEFAULT_FILTER_TEXT);
-		};
-	}).val(DEFAULT_FILTER_TEXT);
-	
 	// /////////////////////////////////////////
 	// /click on a space list item
 
@@ -2246,11 +2230,10 @@ $(document).ready(function() {
 	
 	var loadSpaces = function(spaces,filter) {
 		$("#spaces-list").selectablelist("clear");
-		
 		var firstMatchFound = false;
 		for(s in spaces){
 			var space = spaces[s];
-			if(filter === undefined || filter == DEFAULT_FILTER_TEXT || space.spaceId.toLowerCase().indexOf(filter.toLowerCase()) > -1){
+			if(filter === undefined || space.spaceId.toLowerCase().indexOf(filter.toLowerCase()) > -1){
 				addSpaceToList(space);
 				if(!firstMatchFound){
 					//$("#spaces-list").selectablelist('setCurrentItemById',space.spaceId);	   
@@ -2258,8 +2241,6 @@ $(document).ready(function() {
 				}
 			}
 		}
-		
-		
 	};
 	
 	var refreshSpaces = function(providerId, /*optional*/successFunc){
@@ -2277,7 +2258,7 @@ $(document).ready(function() {
 					spacesArray[s] = {spaceId: spaces[s], storeId: providerId};
 				}
 				// clear content filters
-				$("#content-item-filter").val(DEFAULT_FILTER_TEXT);
+				$("#content-item-filter").val('');
 				loadSpaces(spacesArray, $("#space-filter").val());
 				$("#space-list-status").fadeOut("fast");
 				
