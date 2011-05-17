113b947550d8d6ac6470f99c9b20c107d26b7027
Daniel Bernstein <dbernstein@duraspace.org>
2011-05-17 05:55:24 +0000

    removed obsolete code
    git-svn-id: https://svn.duraspace.org/duracloud/trunk@408 1005ed41-97cd-4a8f-848c-be5b5fe45bcb

--- duradmin/src/main/java/org/duracloud/duradmin/view/BaseViewPreparer.java
+++ duradmin/src/main/java/org/duracloud/duradmin/view/BaseViewPreparer.java
@@ -7,12 +7,10 @@
  */
 package org.duracloud.duradmin.view;
 
-import org.apache.tiles.Attribute;
 import org.apache.tiles.AttributeContext;
 import org.apache.tiles.context.TilesRequestContext;
 import org.apache.tiles.preparer.ViewPreparer;
 import org.duracloud.duradmin.contentstore.ContentStoreProvider;
-import org.duracloud.error.ContentStoreException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,37 +38,6 @@ public class BaseViewPreparer
     public void execute(TilesRequestContext tilesRequestContext,
                         AttributeContext attributeContext) {
 
-    	
-        try {
-        	attributeContext.putAttribute("mainMenu", new Attribute(MainMenu
-                    .instance()), true);
-            log.debug("main menu attribute set");
-
-            String currentUrl =
-                    (String) tilesRequestContext.getRequestScope()
-                            .get("currentUrl");
-            attributeContext.putAttribute("currentUrl",
-                                          new Attribute(currentUrl),
-                                          true);
-
-            log.debug("currentUrl attribute set:" + currentUrl);
-
-            contentStoreProvider.getContentStore();
-
-            attributeContext.putAttribute("contentStoreProvider",
-                                          new Attribute(contentStoreProvider),
-                                          true);
-
-            log.debug("contentStoreProvider attribute set: "
-                    + contentStoreProvider);
-            
-            
-            
-        } catch (ContentStoreException ex) {
-            log.error("failed to complete execution of BaseViewPreparer: "
-                    + ex.getMessage());
-
-        }
 
     }
 }
