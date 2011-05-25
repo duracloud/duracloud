ccc731fb8c4265e80d3db40bbd7fed34f8a639e7
Andrew Woods <awoods@duraspace.org>
2011-05-25 23:05:17 +0000

    This update ensures that the DateUtil class sets times to GMT-4:00 (East Coast time).
    git-svn-id: https://svn.duraspace.org/duracloud/trunk@424 1005ed41-97cd-4a8f-848c-be5b5fe45bcb

--- common/src/main/java/org/duracloud/common/util/DateUtil.java
+++ common/src/main/java/org/duracloud/common/util/DateUtil.java
@@ -10,6 +10,7 @@ package org.duracloud.common.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateUtil {
 
@@ -61,6 +62,7 @@ public class DateUtil {
 
     public static String convertToString(long millis, DateFormat format) {
         SimpleDateFormat dateFormat = format.format;
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT-4:00"));
         return dateFormat.format(millis);
     }
 
