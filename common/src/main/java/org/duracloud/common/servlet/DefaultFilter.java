b31c0165691fbd5b734f10aec2e94ee717810b1e
Daniel Bernstein <dbernstein@duraspace.org>
2012-06-27 05:15:26 +0000

    This update adds a DefaultFilter to the commons project. It is useful for delegating calls for static resources to the default servlet.
    git-svn-id: https://svn.duraspace.org/duracloud/trunk@955 1005ed41-97cd-4a8f-848c-be5b5fe45bcb

---
+++ common/src/main/java/org/duracloud/common/servlet/DefaultFilter.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2009-2012 DuraSpace. All rights reserved.
+ */
+package org.duracloud.common.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @contributor "Daniel Bernstein (dbernstein@duraspace.org)"
+ * 
+ */
+public class DefaultFilter implements Filter {
+
+	private RequestDispatcher defaultRequestDispatcher;
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		defaultRequestDispatcher.forward(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.defaultRequestDispatcher = filterConfig.getServletContext()
+				.getNamedDispatcher("default");
+	}
+}
