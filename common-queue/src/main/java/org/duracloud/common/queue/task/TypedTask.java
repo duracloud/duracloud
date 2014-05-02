54e9b953c429c8e4115c9787f7ae3b9772981db5
Daniel Bernstein <dabylon@gmail.com>
2014-05-02 10:58:02 -0700

    Factors TypedTask into two classes to capture the fact that some tasks are space centric and thus do not require a contentId to function.

--- common-queue/src/main/java/org/duracloud/common/queue/task/TypedTask.java
+++ common-queue/src/main/java/org/duracloud/common/queue/task/TypedTask.java
@@ -12,56 +12,17 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import java.util.Map;
 
 /**
- * Contains the base information necessary to handle tasks.
+ * Contains the base information necessary for tasks working with content items.
  *
- * @author Bill Branan
- *         Date: 10/24/13
+ * @author Daniel Bernstein
+ *         Date: 05/02/2014
  */
-public abstract class TypedTask {
+public abstract class TypedTask extends SpaceCentricTypedTask{
 
-    public static final String ACCOUNT_PROP = "account";
-    public static final String STORE_ID_PROP = "storeId";
-    public static final String SPACE_ID_PROP = "spaceId";
     public static final String CONTENT_ID_PROP = "contentId";
 
-    private String account;
-    private String storeId;
-    private String spaceId;
     private String contentId;
 
-    /* To be used as the value for variables when that variable doesn't apply
-       to the current action type, such for contentId when the action involves
-       only a space */
-    public static final String NA =  "not-applicable";
-
-    /**
-     * The unique identifier for the account, ie the account's subdomain.
-     * 
-     * @return
-     */
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getStoreId() {
-        return storeId;
-    }
-
-    public void setStoreId(String storeId) {
-        this.storeId = storeId;
-    }
-
-    public String getSpaceId() {
-        return spaceId;
-    }
-
-    public void setSpaceId(String spaceId) {
-        this.spaceId = spaceId;
-    }
 
     public String getContentId() {
         return contentId;
@@ -70,41 +31,18 @@ public abstract class TypedTask {
     public void setContentId(String contentId) {
         this.contentId = contentId;
     }
-    
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
 
-    /**
-     * Reads the information stored in a Task and sets data in the TypedTask
-     * @param task
-     */
+    @Override
     public void readTask(Task task) {
+        super.readTask(task);
         Map<String, String> props = task.getProperties();
-        setAccount(props.get(ACCOUNT_PROP));
-        setStoreId(props.get(STORE_ID_PROP));
-        setSpaceId(props.get(SPACE_ID_PROP));
         setContentId(props.get(CONTENT_ID_PROP));
     }
 
-    /**
-     * Writes all of the information in the TypedTask into a Task
-     * @return a Task based on the information stored in this TypedTask
-     */
+    @Override
     public Task writeTask() {
-        Task task = new Task();
-        addProperty(task, ACCOUNT_PROP, getAccount());
-        addProperty(task, STORE_ID_PROP, getStoreId());
-        addProperty(task, SPACE_ID_PROP, getSpaceId());
+        Task task = super.writeTask();
         addProperty(task, CONTENT_ID_PROP, getContentId());
         return task;
     }
-
-    protected void addProperty(Task task, String propName, String propValue) {
-        if(null != propValue && !propValue.equals(NA)) {
-            task.addProperty(propName, propValue);
-        }
-    }
-
 }
