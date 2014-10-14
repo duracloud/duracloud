764214d400945dce5e0bdbf17f8bdd97e45ce451
Bill Branan <bbranan@duraspace.org>
2014-10-14 14:06:22 -0400

    Adds file header to all Java classes where they were missing. Also updates the notice file to include 2014.

--- synctoolui/src/main/java/org/duracloud/syncui/domain/ThreadCountForm.java
+++ synctoolui/src/main/java/org/duracloud/syncui/domain/ThreadCountForm.java
@@ -3,7 +3,7 @@
  * detailed in the LICENSE and NOTICE files at the root of the source
  * tree and available online at
  *
- *     
+ *     http://duracloud.org/license/
  */
 package org.duracloud.syncui.domain;
 
