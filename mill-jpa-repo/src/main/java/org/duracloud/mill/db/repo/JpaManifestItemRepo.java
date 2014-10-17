fce4c90b7feca2cca94a28701b03a730e64eff00
Daniel Bernstein <dabylon@gmail.com>
2014-10-17 10:18:36 -0700

    removes unused import.

--- mill-jpa-repo/src/main/java/org/duracloud/mill/db/repo/JpaManifestItemRepo.java
+++ mill-jpa-repo/src/main/java/org/duracloud/mill/db/repo/JpaManifestItemRepo.java
@@ -13,7 +13,6 @@ import org.duracloud.mill.db.model.ManifestItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
