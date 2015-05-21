e3d5997c53e72db93cb21cd090f5d9bd3564a2f3
Daniel Bernstein <dabylon@gmail.com>
2015-05-21 15:28:50 -0700

    Updates synctoolui to use 1000 rather than 1024 as the divisor for formatting KB, MB, GB, and TBs

--- synctoolui/src/main/java/org/duracloud/syncui/util/FormatUtil.java
+++ synctoolui/src/main/java/org/duracloud/syncui/util/FormatUtil.java
@@ -13,7 +13,7 @@ public class FormatUtil {
     public static String readableFileSize(long size) {
         if(size <= 0) return "0";
         final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
-        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
-        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1000));
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1000, digitGroups)) + " " + units[digitGroups];
     }
 }
