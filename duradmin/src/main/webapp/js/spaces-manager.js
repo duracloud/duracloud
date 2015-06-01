420f00dc14d5aba2b6ed37b6b6470722f68a4a7e
Daniel Bernstein <dabylon@gmail.com>
2015-06-01 15:59:59 -0700

    Resolves isssue #5 in https://wiki.duraspace.org/display/DSPINT/Release+3.3.0+Tasks

--- duradmin/src/main/webapp/js/spaces-manager.js
+++ duradmin/src/main/webapp/js/spaces-manager.js
@@ -568,8 +568,10 @@ $(function() {
           
           var loadWriteableSpaces = function(storeId, spaceId) {
             spaceSelect.busySibling("Loading writable spaces...");
+	    spaceSelect.disable(true);
             dc.store.GetSpaces(storeId, true, {
               success : function(spaces) {
+	        spaceSelect.disable(false);
                 var selectedSpaceId = spaceId ? spaceId : spaceSelect.val();
                 spaceSelect.children().remove();
                 spaceSelect.append("<option value=''>Choose</option>");
@@ -579,7 +581,7 @@ $(function() {
                 spaceSelect.val(selectedSpaceId);
                 spaceSelect.idleSibling();
               },
-            }, true // make
+            }, true// make
             // it an
             // asynchronous
             // call
@@ -590,7 +592,7 @@ $(function() {
           // a select box (ie multiple stores
           // available)
           // load list of spaces
-          destStoreIdField.change(function(evt) {
+          destStoreIdField.unbind().change(function(evt) {
             var dest = this;
             loadWriteableSpaces($(dest).val());
           });
