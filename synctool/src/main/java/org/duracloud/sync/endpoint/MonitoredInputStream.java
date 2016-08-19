a15a89952f10435ee110838ffdff1a2edd462d52
Daniel Bernstein <dabylon@gmail.com>
2016-08-19 12:32:57 -0700

    Partially completes  https://jira.duraspace.org/browse/DURACLOUD-1061: Add multichunk  test coverage for chunked files in the DuraStoreChunkSyncEndpoint. Writing of chunks to disk before passing on ContentStore is slowing down the 3GB file test By a factor of at least 10 so the 3GB test has been abandonned for now.

--- synctool/src/main/java/org/duracloud/sync/endpoint/MonitoredInputStream.java
+++ synctool/src/main/java/org/duracloud/sync/endpoint/MonitoredInputStream.java
@@ -10,19 +10,24 @@ package org.duracloud.sync.endpoint;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FilterInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author: Bill Branan
  * Date: 10/20/11
  */
-public class MonitoredFileInputStream extends FileInputStream {
+public class MonitoredInputStream extends FilterInputStream {
 
     long bytesRead;
 
-    public MonitoredFileInputStream(File file) throws FileNotFoundException {
-        super(file);
-
+    public MonitoredInputStream(File file) throws FileNotFoundException {
+        this(new FileInputStream(file));
+    }
+    
+    public MonitoredInputStream(InputStream is)  {
+        super(is);
         this.bytesRead = 0;
     }
 
@@ -37,7 +42,7 @@ public class MonitoredFileInputStream extends FileInputStream {
 
     @Override
     public int read(byte[] b) throws IOException {
-        return updateBytesRead(super.read(b));
+        return read(b, 0, b.length);
     }
 
     @Override
