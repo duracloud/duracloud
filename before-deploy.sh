0740c7062d2a79fabe405c9a961bf9e09d557e91
Bill Branan <bbranan@duraspace.org>
2017-08-07 11:23:41 -0400

    Replaces code signing key with one encrypted on linux rather than windows

--- before-deploy.sh
+++ before-deploy.sh
@@ -2,8 +2,8 @@
 echo 'Starting before-deploy.sh'
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-        openssl aes-256-cbc -K $encrypted_fe4a3fa1215b_key -iv $encrypted_fe4a3fa1215b_iv -in codesigning.asc.enc -out codesigning.asc -d
-        gpg --fast-import codesigning.asc
+        openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in codesignkey.asc.enc -out codesignkey.asc -d
+        gpg --fast-import codesignkey.asc
         echo 'Completed before-deploy.sh'
     fi
 fi
