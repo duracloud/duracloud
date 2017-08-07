4caa6ee844f878c814fba9a8821692c8f933199a
Bill Branan <bbranan@duraspace.org>
2017-08-07 12:29:24 -0400

    Add batch-mode flag to maven build to reduce build log length (which caused Travis build to fail)

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo 'Starting deploy.sh'
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-        mvn deploy -DreleaseBuild -DskipTests -DskipDeploy --settings mvndeploy-settings.xml
+        mvn deploy -DreleaseBuild -DskipTests -DskipDeploy --settings mvndeploy-settings.xml --batch-mode
         echo 'Completed deploy.sh'
   fi
 fi
