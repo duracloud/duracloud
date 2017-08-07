11d66ea31b466abef1415f53ff916dc4e8b78e98
Bill Branan <bbranan@duraspace.org>
2017-08-07 17:55:31 -0400

    Another attempt to move auto-deploy files to /resources/travis

--- resources/travis/before-deploy.sh
+++ resources/travis/before-deploy.sh
@@ -2,7 +2,7 @@
 echo 'Starting before-deploy.sh'
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-        openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in codesignkey.asc.enc -out codesignkey.asc -d
+        openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in resources/travis/codesignkey.asc.enc -out codesignkey.asc -d
         gpg --fast-import codesignkey.asc
         echo 'Completed before-deploy.sh'
     fi
--- resources/travis/deploy.sh
+++ resources/travis/deploy.sh
@@ -2,7 +2,7 @@
 echo 'Starting deploy.sh'
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-        mvn deploy -DreleaseBuild -DskipTests -DskipDeploy --settings mvndeploy-settings.xml --batch-mode
+        mvn deploy -DreleaseBuild -DskipTests -DskipDeploy --settings resources/travis/mvndeploy-settings.xml --batch-mode
         echo 'Completed deploy.sh'
   fi
 fi
