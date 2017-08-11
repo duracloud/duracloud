a504e316c9a6cc578cb7d62a6b4665d1cb981449
dbernstein <dabylon@gmail.com>
2017-08-11 21:13:46 +0000

    Adds support for correctly generating beanstalk zips when branch is 'develop' or a tag is set.

--- resources/travis/before-deploy.sh
+++ resources/travis/before-deploy.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+
+# function that generates a beanstalk zip based on twp params: s3 bucket and zip file.
+generateBeanstalkZip ()
+{
+   echo "Generating beanstalk zip"
+   s3Bucket=$1
+   echo "s3Bucket = ${s3Bucket}"
+   zipFile=$2
+   echo "zipFile=${zipFile}"
+
+    echo "stage beanstalk package"
+    mvn process-resources -Ds3.config.bucket=${s3Bucket}  --non-recursive
+    cd target
+    zip -r ${zipFile} duradmin.war durastore.war ROOT.war .ebextensions
+    cd ..
+}
+
 echo 'Starting before-deploy.sh'
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
@@ -8,10 +26,17 @@ if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     fi
 fi
 
+if [ "$TRAVIS_BRANCH" = 'develop' ]; then
+   currentGitCommit=`git rev-parse HEAD`;
+   projectVersion=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
+   echo "generating beanstalk zip for $projectVersion ${currentGitCommit}..."
+   generateBeanstalkZip "duracloud-dev-config" "duracloud-dev-beanstalk-$projectVersion-${currentGitCommit:0:7}.zip"
+fi
+
 
 echo TRAVIS_TAG=$TRAVIS_TAG
 
-if [ "$TRAVIS_TAG" != null]; then 
+if [ ! -z "$TRAVIS_TAG" ]; then 
     LOCAL_INSTALL=target/install
     mkdir -p  $LOCAL_INSTALL
     echo "stage install package"
@@ -21,20 +46,19 @@ if [ "$TRAVIS_TAG" != null]; then
     cp target/duradmin.war ${LOCAL_INSTALL}/
     echo "zip installation package"
     package="installation-package-${TRAVIS_TAG}.zip"
-    zip -r -j ${package} $LOCAL_INSTALL/
+    zip -r -j target/${package} $LOCAL_INSTALL/
 
-    echo "stage beanstalk package"
-    cd target
-    zip -r duracloud-beanstalk-release-${TRAVIS_TAG}.zip duradmin.war durastore.war ROOT.war .ebextensions
+    #echo "Generating installers..."
+    #mvn clean install -Pinstallers -pl synctoolui
 
-    cd ..
+    generateBeanstalkZip "duracloud-production-config" duracloud-production-beanstalk-${TRAVIS_TAG}.zip
+    
     echo "Generating  javadocs..."
-    mvn javadoc:aggregate -Dadditionalparam="-Xdoclint:none" -Pjava8-disable-strict-javadoc
+    # the irodsstorageprovider is excluded due to maven complaining about it. This exclusion will likely be temporary.
+    mvn javadoc:aggregate -Dadditionalparam="-Xdoclint:none" -Pjava8-disable-strict-javadoc  -pl \!irodsstorageprovider
     cd target/site/apidocs
     zipFile=duracloud-${TRAVIS_TAG}-apidocs.zip
     echo "Zipping javadocs..."
-    zip -r duracloud-${TRAVIS_TAG}-apidocs.zip .
-     
+    zip -r ${zipFile} .
 fi
-
 
