e03e26287df8c909d03dcf5003fe0ac5cd74c5f1
Daniel Bernstein <dabylon@gmail.com>
2017-08-14 17:09:11 -0700

    Fixes echo statement by replacing backticks with quotes.

--- resources/travis/before-deploy.sh
+++ resources/travis/before-deploy.sh
@@ -2,7 +2,7 @@
 echo 'Starting before-deploy.sh'
 if [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-        echo `Decrypting code signing key`
+        echo "Decrypting code signing key"
         openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in resources/travis/codesignkey.asc.enc -out codesignkey.asc -d
         gpg --fast-import codesignkey.asc
     fi
