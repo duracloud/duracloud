65da4cb54aa4f5b7fbf49284ff7922e1ae8994b0
Daniel Bernstein <dabylon@gmail.com>
2017-10-16 09:53:43 -0700

    Ensures thread safety of DuraStoreRetrievalSource and DuraStoreStitchingRetrievalSource.
    Resolves: https://jira.duraspace.org/browse/DURACLOUD-1152

--- retrievaltool/src/main/java/org/duracloud/retrieval/source/DuraStoreStitchingRetrievalSource.java
+++ retrievaltool/src/main/java/org/duracloud/retrieval/source/DuraStoreStitchingRetrievalSource.java
@@ -48,7 +48,7 @@ public class DuraStoreStitchingRetrievalSource extends DuraStoreRetrievalSource 
     }
 
     @Override
-    public ContentItem getNextContentItem() {
+    public synchronized ContentItem getNextContentItem() {
         log.debug("enter getNextContentItem()");
         ContentItem item = super.getNextContentItem();
 
