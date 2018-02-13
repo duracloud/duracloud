ce3f7eafa13a549c4ff3f1431ecc447c3371f9b5
Bill Branan <bbranan@duraspace.org>
2018-02-13 17:19:33 -0500

    Updates InstallBuilder version to 17.12.0
    Also changes download path to pull from DuraCloud (dcprod) so as to ensure that a consistent version is retrieved

--- resources/travis/before-install.sh
+++ resources/travis/before-install.sh
@@ -2,11 +2,11 @@
 echo 'Starting before-install.sh'
 if [ ! -z "$TRAVIS_TAG" ] || [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     echo "Installing BitRock InstallBuilder"
-    wget https://installbuilder.bitrock.com/installbuilder-enterprise-17.9.0-linux-x64-installer.run
-    chmod +x installbuilder-enterprise-17.9.0-linux-x64-installer.run
+    wget https://dcprod.duracloud.org/durastore/resources/installbuilder-enterprise-17.12.0-linux-x64-installer.run
+    chmod +x installbuilder-enterprise-17.12.0-linux-x64-installer.run
     # Using sudo to ensure installbuilder is installed to /opt rather than under /home
-    sudo ./installbuilder-enterprise-17.9.0-linux-x64-installer.run --mode unattended
+    sudo ./installbuilder-enterprise-17.12.0-linux-x64-installer.run --mode unattended
     # Add license file
-    sudo openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in resources/travis/installbuilder-license.xml.enc -out /opt/installbuilder-17.9.0/license.xml -d
+    sudo openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in resources/travis/installbuilder-license.xml.enc -out /opt/installbuilder-17.12.0/license.xml -d
     echo 'Completed before-install.sh'
 fi
