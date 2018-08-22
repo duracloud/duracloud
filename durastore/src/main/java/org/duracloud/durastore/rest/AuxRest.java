1de5f65e69af29f4c18ec503b39d20011f86b6ec
dbernstein <dabylon@gmail.com>
2018-08-22 12:52:47 -0700

    Adds unit tests for AuxRest, new S3StorageProvider methods, and StringDataStore (#72)

--- durastore/src/main/java/org/duracloud/durastore/rest/AuxRest.java
+++ durastore/src/main/java/org/duracloud/durastore/rest/AuxRest.java
@@ -18,18 +18,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.duracloud.common.constant.Constants;
-import org.duracloud.common.error.DuraCloudRuntimeException;
-import org.duracloud.common.rest.RestUtil;
-import org.duracloud.s3storage.S3StorageProvider;
 import org.duracloud.s3storage.StringDataStore;
+import org.duracloud.s3storage.StringDataStoreFactory;
 import org.duracloud.s3storageprovider.dto.SignedCookieData;
-import org.duracloud.storage.domain.StorageAccount;
-import org.duracloud.storage.domain.StorageAccountManager;
-import org.duracloud.storage.domain.StorageProviderType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -42,16 +37,12 @@ import org.springframework.stereotype.Component;
 public class AuxRest extends BaseRest {
     private final Logger log = LoggerFactory.getLogger(AuxRest.class);
 
-    private RestUtil restUtil;
-
-    private StringDataStore dataStore;
-
     @Inject
-    private StorageAccountManager storageAccountManager;
+    private StringDataStoreFactory stringDataStoreFactory;
 
-    @Autowired
-    public AuxRest(RestUtil restUtil) {
-        this.restUtil = restUtil;
+    @VisibleForTesting
+    protected void setStringDataStoreFactory(StringDataStoreFactory stringDataStoreFactory) {
+        this.stringDataStoreFactory = stringDataStoreFactory;
     }
 
     /**
@@ -65,9 +56,7 @@ public class AuxRest extends BaseRest {
     @GET
     public Response getCookies(@QueryParam("token") String token) {
         try {
-            S3StorageProvider s3StorageProvider = getS3StorageProvider();
-
-            StringDataStore dataStore = new StringDataStore(Constants.HIDDEN_COOKIE_SPACE, s3StorageProvider);
+            StringDataStore dataStore = this.stringDataStoreFactory.create(Constants.HIDDEN_COOKIE_SPACE);
             String cookiesData = dataStore.retrieveData(token);
 
             if (null == cookiesData) {
@@ -97,19 +86,8 @@ public class AuxRest extends BaseRest {
 
             return Response.ok(html, MediaType.TEXT_HTML_TYPE)
                            .cookie(responseCookies.toArray(new NewCookie[responseCookies.size()])).build();
-
         } catch (Exception e) {
             return responseBad(e);
         }
     }
-
-    private S3StorageProvider getS3StorageProvider() {
-        if (!storageAccountManager.isInitialized()) {
-            throw new DuraCloudRuntimeException("storageAccountManager is not initialized!!!");
-        }
-        StorageAccount account = storageAccountManager.getPrimaryStorageAccount();
-        StorageProviderType type = account.getType();
-        return new S3StorageProvider(account.getUsername(), account.getPassword(), account.getOptions());
-    }
-
 }
