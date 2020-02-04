0e0054391413e8629ea60b61bdf488910f87ef46
dbernstein <dabylon@gmail.com>
2020-02-04 08:46:00 -0800

    DURACLOUD-1247: Updates the SyncTool to support 64-bit MacOS (PR #110)
    Resolves:  https://jira.lyrasis.org/browse/DURACLOUD-1247
    * Ensures that InstallBuilder builds a 64bit installer application on MacOS.
    * Updates installbuilder version in travis.

--- resources/travis/before-install.sh
+++ resources/travis/before-install.sh
@@ -2,11 +2,13 @@
 echo 'Starting before-install.sh'
 if [ ! -z "$TRAVIS_TAG" ] || [ "$TRAVIS_BRANCH" = 'master' ] || [ "$TRAVIS_BRANCH" = 'develop' ]; then
     echo "Installing BitRock InstallBuilder"
-    wget https://dcprod.duracloud.org/durastore/resources/installbuilder-enterprise-17.12.0-linux-x64-installer.run
-    chmod +x installbuilder-enterprise-17.12.0-linux-x64-installer.run
+    installBuilderVersion = "19.12.0"
+    installBuilderInstaller = "installbuilder-enterprise-${installBuilderVersion}-linux-x64-installer.run"
+    wget https://dcprod.duracloud.org/durastore/resources/${installBuilderInstaller}
+    chmod +x ${installBuilderInstaller} 
     # Using sudo to ensure installbuilder is installed to /opt rather than under /home
-    sudo ./installbuilder-enterprise-17.12.0-linux-x64-installer.run --mode unattended
+    sudo ./${installBuilderInstaller} --mode unattended
     # Add license file
-    sudo openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in resources/travis/installbuilder-license.xml.enc -out /opt/installbuilder-17.12.0/license.xml -d
+    sudo openssl aes-256-cbc -K $encrypted_01c7144b0525_key -iv $encrypted_01c7144b0525_iv -in resources/travis/installbuilder-license.xml.enc -out /opt/installbuilder-${installBuilderVersion}/license.xml -d
     echo 'Completed before-install.sh'
 fi
