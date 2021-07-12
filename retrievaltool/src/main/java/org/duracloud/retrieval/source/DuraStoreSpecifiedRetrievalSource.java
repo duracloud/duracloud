9a6035b07533f5846baf8e15f365180e4bf9eb29
nwoodward <woodward.nicholas@gmail.com>
2021-07-12 10:33:00 -0500

    refactor DuraStoreSpecifiedRetrievalSource to create full list of contentIds, including chunked content

--- retrievaltool/src/main/java/org/duracloud/retrieval/source/DuraStoreSpecifiedRetrievalSource.java
+++ retrievaltool/src/main/java/org/duracloud/retrieval/source/DuraStoreSpecifiedRetrievalSource.java
@@ -7,15 +7,27 @@
  */
 package org.duracloud.retrieval.source;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.duracloud.chunk.manifest.ChunksManifest;
 import org.duracloud.client.ContentStore;
+import org.duracloud.common.constant.ManifestFormat;
 import org.duracloud.common.error.DuraCloudRuntimeException;
 import org.duracloud.common.model.ContentItem;
 import org.duracloud.domain.Content;
 import org.duracloud.error.ContentStoreException;
+import org.duracloud.manifest.ManifestFormatter;
+import org.duracloud.manifest.impl.ManifestFormatterFactory;
+import org.duracloud.mill.db.model.ManifestItem;
 import org.duracloud.retrieval.mgmt.RetrievalListener;
 import org.duracloud.stitch.error.MissingContentException;
 import org.slf4j.Logger;
@@ -37,6 +49,7 @@ public class DuraStoreSpecifiedRetrievalSource extends DuraStoreStitchingRetriev
     private final Logger log = LoggerFactory.getLogger(
         DuraStoreSpecifiedRetrievalSource.class);
 
+
     private Iterator<String> specifiedContentIds;
 
     public DuraStoreSpecifiedRetrievalSource(ContentStore store,
@@ -55,6 +68,81 @@ public class DuraStoreSpecifiedRetrievalSource extends DuraStoreStitchingRetriev
         }
 
         this.specifiedContentIds = specifiedContentIds;
+        this.reviewSpecifiedContentIdsForChunkedContent(singleSpaceList);
+    }
+
+    protected void reviewSpecifiedContentIdsForChunkedContent(List<String> singleSpaceList) {
+        log.debug("enter remediateSpecifiedContentIds()");
+
+        List<String> retrievalContentIds = new ArrayList<String>();
+        while (specifiedContentIds.hasNext()) {
+            String temp = specifiedContentIds.next();
+            retrievalContentIds.add(temp);
+        }
+        log.debug("total contentIds in list-file: " + retrievalContentIds.size());
+
+        List<String> retrievalSpaceContentIds = new ArrayList<String>();
+
+        Iterator<String> retrievalSpaceIds = verifySpaceIds(singleSpaceList);
+        if (retrievalSpaceIds.hasNext()) {
+            String currentRetrievalSpaceId = retrievalSpaceIds.next();
+            log.debug("searching for contentIds in space: " + currentRetrievalSpaceId);
+            try {
+                InputStream manifest = contentStore.getManifest(currentRetrievalSpaceId, ManifestFormat.TSV);
+                ManifestFormatter formatter = new ManifestFormatterFactory().create(ManifestFormat.TSV);
+                String header = formatter.getHeader();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(manifest));
+                String line = null;
+                ManifestItem item = null;
+
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        // ignore any whitespace
+                        if (line.trim().length() == 0) {
+                            continue;
+                        }
+
+                        // ignore header line
+                        if (line.equals(header)) {
+                            continue;
+                        }
+
+                        try {
+                            item = formatter.parseLine(line);
+                        } catch (ParseException e) {
+                            throw new IOException(e);
+                        }
+
+                        String contentId = item.getContentId();
+
+                        // search for chunks or chunk manifests for contentId
+                        String tempContentId = contentId;
+                        if (contentId.endsWith(ChunksManifest.manifestSuffix)) {
+                            tempContentId = StringUtils.removeEnd(contentId, ChunksManifest.manifestSuffix);
+                        } else if (contentId.contains(ChunksManifest.chunkSuffix)) {
+                            tempContentId = contentId.substring(0, contentId.indexOf(ChunksManifest.chunkSuffix));
+                        }
+
+                        if (retrievalContentIds.contains(tempContentId)) {
+                            log.debug("found chunked content for contentId in list-file: " + contentId);
+                            retrievalSpaceContentIds.add(contentId);
+                        }
+                    }
+                } catch (IOException ex) {
+                    log.error("Error reading space manifest.");
+                }
+            } catch (ContentStoreException cse) {
+                log.error("Unable to retrieve space manifest. If files-list.txt contains chunked files and the " +
+                          "retrieval fails the local content dir will need to be empty.");
+            }
+        }
+
+        if (!retrievalSpaceContentIds.isEmpty()) {
+            Collections.sort(retrievalSpaceContentIds);
+            log.debug("List of contentIds to retrieve has increased from " + retrievalContentIds.size() + " to " +
+                     retrievalSpaceContentIds.size() + " after chunked content is included.");
+            this.specifiedContentIds = retrievalSpaceContentIds.iterator();
+        }
     }
 
     @Override
@@ -62,27 +150,6 @@ public class DuraStoreSpecifiedRetrievalSource extends DuraStoreStitchingRetriev
         if (spaceIds.hasNext()) {
             currentSpaceId = spaceIds.next();
             currentContentList = specifiedContentIds;
-        }
-    }
-
-    @Override
-    protected Content doGetContent(ContentItem item, RetrievalListener listener) {
-        try {
-            return contentStore.getContent(item.getSpaceId(),
-                                           item.getContentId());
-        } catch (ContentStoreException cse) {
-            log.info("Error retrieving content ID: " + item.getContentId() +
-                     ".  Trying to get this content again by checking for " +
-                     "a chunk manifest for this content ID.");
-            // Create a new ContentItem representing the manifest file content ID
-            // for the passed in ContentItem to this method.
-            ContentItem manifestItem = new ContentItem(item.getSpaceId(),
-                                                       item.getContentId() + ChunksManifest.manifestSuffix);
-            try {
-                return doGetContentFromManifest(manifestItem, listener);
-            } catch (MissingContentException mse) {
-                throw mse;
-            }
         }
     }
 }
