df740d1953712d1c20f725b8ab3a48e2a42e9a59
Michael Ritter <mikejritter@users.noreply.github.com>
2026-06-01 10:18:05 -0600

    Release 8.2.1 (#210)
    * Disable Chronopolis Snapshots (#209)

--- duradmin/src/main/webapp/jquery/dc/widget/ui.snapshot.js
+++ duradmin/src/main/webapp/jquery/dc/widget/ui.snapshot.js
@@ -14,25 +14,25 @@
 
     $.widget("ui.snapshot",
 	$.extend({}, $.ui.expandopanel.prototype, 
-		{  
+		{
 	        _listPanel: null, 
 	        _init: function(){ 
 				$.ui.expandopanel.prototype._init.call(this); //call super init first
 				var that = this;
                 this.getContent().append(this._createUI());
-				
-			}, 
-			
+
+			},
+
 			destroy: function(){ 
 				//tabular destroy here
 				$.ui.expandopanel.prototype.destroy.call(this); // call the original function 
-			}, 
+			},
 
 			options: $.extend({}, $.ui.expandopanel.prototype.options, {
 				title: "Snapshot",
 			    space: null,
 			}),
-			
+
 			_displayProperties: function(properties, panel){
 			    panel.empty();
 			    panel.removeClass()
@@ -51,7 +51,7 @@
 
 			        list.push([i, value]);
 			    }
-			    
+
           panel.append(dc.createTable(list));
           $(panel).find("#snapshot-id").click(function(evt){
               $(document).trigger("navigateToSpace", {
@@ -60,93 +60,14 @@
               });
           });
 			},
-			
+
 			_createUI: function () {
 			    var that = this;
 			    var s = that.options.space;
                 var form;
 			    var panel = $.fn.create("div");
 			    panel.empty();
-			    if(!s.snapshotInProgress){
-                    var d = $("#snapshot-dialog");
-                    
-                    d.dialog({
-                        autoOpen: false,
-                        show: 'blind',
-                        hide: 'blind',
-                        height: 350,
-                        resizable: false,
-                        closeOnEscape:true,
-                        modal: true,
-                        width:550,
-                        buttons: {
-                            'Create': function(){
-                                var data = form.serialize();
-                                data += "&spaceId="+ s.spaceId
-                                        + "&storeId="+ s.storeId;
-                                
-                                $(this).dialog('close');
-                                dc.busy("Requesting space snapshot...", {modal: true});
-                                dc.store
-                                    .CreateSnapshot(data)
-                                    .always(function(){
-                                        dc.done();
-                                    })
-                                    .done(function(response){
-                                        that._displayProperties(response,panel);
-                                        response.snapshot = false;
-                                        response.storeId = s.storeId;
-                                        response.spaceId = s.spaceId;
-                                        response.forceRefresh = true;
-                                        $(document).trigger("staleSpace", response);
-                                        dc.done("The snapshot is being generated!");
-
-                                    })
-                                    .error(function( jqXHR, 
-                                                    textStatus, 
-                                                    errorThrown ) {
-                                        dc.done();
-
-                                        var message = SnapshotErrorMessage.UNAVAILABLE;
-
-                                        var responseText = jqXHR.responseText;
-
-                                        if(jqXHR.status == 409){
-                                        	message = responseText;
-                                        }
-
-                                        if(responseText.startsWith("{")){
-                                        	var json = JSON.parse(responseText);
-                                        	if(json['exception.message']){
-                                                message = json['exception.message'];
-                                            }
-                                        }
-
-                                        dc.displayErrorDialog(jqXHR,
-                                                              message,
-                                                              null,
-                                                              false);
-                                    });
-                            },
-                            Cancel: function() {
-                                $(this).dialog('close');
-                            }
-                        },
-                        close: function() {
-                        },
-                        open: function(){
-                            form = $("#snapshot-properties-form");
-                            form.clearForm();
-                        }
-                    });
-
-                    var btn = $("<button>Create Snapshot</button>").click(function(e){
-                         d.dialog("open");
-	                 });
-	                 
-			        panel.append(btn);
-	                panel.append("<span class='info'>Invoking this feature will render this space read only.</span>");
-			    }else{
+			    if(s.snapshotInProgress){
 			        dc.store
 			          .GetSnapshotProperties(s.storeId, s.spaceId)
 	                  .done(function(properties){
@@ -159,7 +80,7 @@
 	                                          false);
 	                  });
 			    }
-			    
+
 			    return panel;
 			},
 		}
